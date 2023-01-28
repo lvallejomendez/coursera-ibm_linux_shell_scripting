3c825402147ca8819cdbebe8304881fb6b19318d
Luis Vallejo <57255860+lvallejomendez@users.noreply.github.com>
2023-01-28 22:52:53 +0100

    updated file

--- backup.sh
+++ backup.sh
@@ -27,7 +27,7 @@ echo "The destination directory is $destinationDirectory"
 currentTS=`date +%s`
 
 # [TASK 4]
-backupFileName="backup-[$currentTS].tar.gz"
+backupFileName="backup-$currentTS.tar.gz"
 
 # We're going to:
   # 1: Go into the target directory
